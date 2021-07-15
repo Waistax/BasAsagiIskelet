ec95e7304385f1dabb3caac1340cfa39a6caa9d8
Cem GEÇGEL <aa-albara@hotmail.com>
2021-07-15 18:23:30 +0300

    2.8.6
    Modül bilgisi güncellendi.
    Fonksiyon olarak bir değişiklik yapılmadı ama temizlendi.

--- kod/module-info.java
+++ kod/module-info.java
@@ -17,20 +17,24 @@ module başaşağıderebeyi.iskelet {
 	
 	requires transitive başaşağıderebeyi.kütüphane.arayüz;
 	requires transitive başaşağıderebeyi.kütüphane.deney;
+	requires transitive başaşağıderebeyi.kütüphane.matematik;
 	requires transitive başaşağıderebeyi.kütüphane.matematik.dikdörtgen;
 	requires transitive başaşağıderebeyi.kütüphane.matematik.doğrusalcebir;
+	requires transitive başaşağıderebeyi.kütüphane.matematik.ölçüm;
+	requires transitive başaşağıderebeyi.kütüphane.matematik.yerleşim;
 	requires transitive başaşağıderebeyi.kütüphane.girdi;
 	requires transitive başaşağıderebeyi.kütüphane.günlük;
-	requires transitive başaşağıderebeyi.kütüphane.matematik;
 	requires transitive başaşağıderebeyi.kütüphane.olay;
-	requires transitive başaşağıderebeyi.kütüphane.matematik.ölçüm;
 	requires transitive başaşağıderebeyi.kütüphane.varlık;
-	requires transitive başaşağıderebeyi.kütüphane.matematik.yerleşim;
 	
-	requires transitive org.lwjgl.natives;
-	requires transitive org.lwjgl.glfw.natives;
-	requires transitive org.lwjgl.opengl.natives;
-	requires transitive org.lwjgl.openal.natives;
+	requires transitive org.lwjgl;
+	requires org.lwjgl.natives;
+	requires transitive org.lwjgl.glfw;
+	requires org.lwjgl.glfw.natives;
+	requires transitive org.lwjgl.opengl;
+	requires org.lwjgl.opengl.natives;
+	requires transitive org.lwjgl.openal;
+	requires org.lwjgl.openal.natives;
 	
 	requires java.desktop;
 }
