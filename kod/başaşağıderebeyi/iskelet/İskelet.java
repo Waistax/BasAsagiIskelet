94656ed9f1af0ca188df752efc775c82071de382
Cem GEÇGEL <aa-albara@hotmail.com>
2021-07-17 13:28:43 +0300

    2.11.0
    İskeletin çalışan bir sürümü.

--- kod/başaşağıderebeyi/iskelet/İskelet.java
+++ kod/başaşağıderebeyi/iskelet/İskelet.java
@@ -28,9 +28,9 @@ public class İskelet {
 	/** Ana sürümü. */
 	public static final int ANA_SÜRÜMÜ = 2;
 	/** Ara sürümü. */
-	public static final int ARA_SÜRÜMÜ = 10;
+	public static final int ARA_SÜRÜMÜ = 11;
 	/** Yaması. */
-	public static final int YAMASI = 14;
+	public static final int YAMASI = 0;
 	/** Bütün sürümü. */
 	public static final String SÜRÜM =
 		ANA_SÜRÜMÜ + "." + ARA_SÜRÜMÜ + "." + YAMASI;
